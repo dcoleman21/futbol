38a780cbccee5afdaf41564f7239830dcd2b100f
Michael Evans <michaeljevans113@gmail.com>
2020-05-25 10:47:07 -0600

    Add test for attributes

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -12,4 +12,10 @@ class StatTrackerTest < MiniTest::Test
   def test_it_exists
     assert_instance_of StatTracker, @stat_tracker
   end
+
+  def test_it_has_readable_attributes
+    assert_equal './data/game.csv', @stat_tracker.games
+    assert_equal './data/teams.csv', @stat_tracker.teams
+    assert_equal './data/game_teams.csv', @stat_tracker.game_teams
+  end
 end

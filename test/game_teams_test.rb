1ea67a0e9e024aede604c5a2aca0f0d1bdde60b0
“Neeru <neeram85@gmail.com>
2020-05-25 14:01:56 -0600

    Tested data for GameTeams class

--- test/game_teams_test.rb
+++ test/game_teams_test.rb
@@ -28,6 +28,24 @@ def test_it_exists
   assert_instance_of GameTeams, @game_teams
 end
 
+def test_it_has_data
+  assert_equal 2012030221, @game_teams.game_id
+  assert_equal 3, @game_teams.team_id
+  assert_equal "away", @game_teams.hoa
+  assert_equal "LOSS", @game_teams.result
+  assert_equal "OT", @game_teams.settled_in
+  assert_equal "John Tortorella", @game_teams.head_coach
+  assert_equal 2, @game_teams.goals
+  assert_equal 8, @game_teams.shots
+  assert_equal 44, @game_teams.tackles
+  assert_equal 8, @game_teams.pim
+  assert_equal 3, @game_teams.power_play_opportunities
+  assert_equal 0, @game_teams.power_play_goals
+  assert_equal 44.8, @game_teams.face_off_win_percentage
+  assert_equal 17, @game_teams.giveaways
+  assert_equal 7, @game_teams.takeaways
+end
+
 
 
 

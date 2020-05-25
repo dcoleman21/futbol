fd5475e2459b2f4457a0947b0c2d83e885cc88b6
Michael Evans <michaeljevans113@gmail.com>
2020-05-25 10:47:53 -0600

    Add initialize method and attr_reader for games, teams, and game_teams data

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,5 +1,17 @@
 class StatTracker
-  def self.from_csv(locations)
-    StatTracker.new
+  attr_reader :games, :teams, :game_teams
+
+  def self.from_csv(data)
+    @games      = data[:games]
+    @teams      = data[:teams]
+    @game_teams = data[:game_teams]
+
+    StatTracker.new(@games, @teams, @game_teams)
+  end
+
+  def initialize(games, teams, game_teams)
+    @games      = games
+    @teams      = teams
+    @game_teams = game_teams
   end
 end

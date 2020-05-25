5e31c9a9d1976abd816d0ead22aae4db8b0b7c80
Michael Evans <62968371+mjevans113@users.noreply.github.com>
2020-05-25 14:06:14 -0600

    Indentation
    great work Neeru, just fixed indentation

--- test/game_teams_test.rb
+++ test/game_teams_test.rb
@@ -4,49 +4,45 @@ require './lib/game_teams'
 require 'pry'
 
 class GameTeamsTest < Minitest::Test
-def setup
-  @game_teams = GameTeams.new({
-    game_id: 2012030221,
-    team_id: 3,
-    hoa: "away",
-    result: "LOSS",
-    settled_in: "OT",
-    head_coach: "John Tortorella",
-    goals: 2,
-    shots: 8,
-    tackles: 44,
-    pim: 8,
-    power_play_opportunities: 3,
-    power_play_goals: 0,
-    face_off_win_percentage: 44.8,
-    giveaways: 17,
-    takeaways: 7
-    })
-end
+  def setup
+    @game_teams = GameTeams.new({
+      game_id: 2012030221,
+      team_id: 3,
+      hoa: "away",
+      result: "LOSS",
+      settled_in: "OT",
+      head_coach: "John Tortorella",
+      goals: 2,
+      shots: 8,
+      tackles: 44,
+      pim: 8,
+      power_play_opportunities: 3,
+      power_play_goals: 0,
+      face_off_win_percentage: 44.8,
+      giveaways: 17,
+      takeaways: 7
+      })
+  end
 
-def test_it_exists
-  assert_instance_of GameTeams, @game_teams
-end
+  def test_it_exists
+    assert_instance_of GameTeams, @game_teams
+  end
 
-def test_it_has_data
-  assert_equal 2012030221, @game_teams.game_id
-  assert_equal 3, @game_teams.team_id
-  assert_equal "away", @game_teams.hoa
-  assert_equal "LOSS", @game_teams.result
-  assert_equal "OT", @game_teams.settled_in
-  assert_equal "John Tortorella", @game_teams.head_coach
-  assert_equal 2, @game_teams.goals
-  assert_equal 8, @game_teams.shots
-  assert_equal 44, @game_teams.tackles
-  assert_equal 8, @game_teams.pim
-  assert_equal 3, @game_teams.power_play_opportunities
-  assert_equal 0, @game_teams.power_play_goals
-  assert_equal 44.8, @game_teams.face_off_win_percentage
-  assert_equal 17, @game_teams.giveaways
-  assert_equal 7, @game_teams.takeaways
-end
-
-
-
-
+  def test_it_has_data
+    assert_equal 2012030221, @game_teams.game_id
+    assert_equal 3, @game_teams.team_id
+    assert_equal "away", @game_teams.hoa
+    assert_equal "LOSS", @game_teams.result
+    assert_equal "OT", @game_teams.settled_in
+    assert_equal "John Tortorella", @game_teams.head_coach
+    assert_equal 2, @game_teams.goals
+    assert_equal 8, @game_teams.shots
+    assert_equal 44, @game_teams.tackles
+    assert_equal 8, @game_teams.pim
+    assert_equal 3, @game_teams.power_play_opportunities
+    assert_equal 0, @game_teams.power_play_goals
+    assert_equal 44.8, @game_teams.face_off_win_percentage
+    assert_equal 17, @game_teams.giveaways
+    assert_equal 7, @game_teams.takeaways
+  end
 end

ce1f112b62f31d489aa81f5275c522c7b9a18bfc
Dani Coleman <danicoleman00@gmail.com>
2020-05-25 13:26:08 -0600

    Add methods and instance variables

--- lib/teams.rb
+++ lib/teams.rb
@@ -1,8 +1,19 @@
 class Teams
-  attr_reader :team_id 
+  attr_reader :team_id,
+              :franchise_id,
+              :team_name,
+              :abbreviation,
+              :stadium,
+              :link
+
+
   def initialize(data)
-
+    @team_id = data[:team_id]
+    @franchise_id = data[:franchise_id]
+    @team_name = data[:team_name]
+    @abbreviation = data[:abbreviation]
+    @stadium = data[:stadium]
+    @link = data[:link]
   end
-
 
 end

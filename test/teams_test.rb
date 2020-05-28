6075d9c3fe58180e9419527bb480889539c7e6d4
Dani Coleman <danicoleman00@gmail.com>
2020-05-27 19:27:49 -0600

    Fixed the set up hash keys

--- test/teams_test.rb
+++ test/teams_test.rb
@@ -7,8 +7,8 @@ class TeamsTest < MiniTest::Test
 
   def setup
       @teams = Teams.new({team_id: 1,
-                         franchise_id: 23,
-                         team_name: "Atlanta United",
+                         franchiseid: 23,
+                         teamname: "Atlanta United",
                          abbreviation: "ATL",
                          stadium: "Mercedes-Benz Stadium",
                          link: "/api/v1/teams/1"

8a867086fb8b60a1dc36ea4d9c2e7caa69e6f05f
James Belta <james.belta@gmail.com>
2020-05-27 18:14:23 -0600

    Access to csv

--- lib/games_collection.rb
+++ lib/games_collection.rb
@@ -1,8 +1,9 @@
 require 'CSV'
 
-class GamesCollection 
-  attr_reader :games
-  def initialize(data)
-    @games = data
+class GamesCollection < Games
+  attr_reader :games, :collection
+  def initialize(path)
+    @games = CSV.read(path, headers: true, header_converters: :symbol)
+    @collection = games.map {|row| Games.new(row)}
   end
 end

0315d2c06a9a6b206b9e28ec0b9ff618171e6e38
Michael Evans <michaeljevans113@gmail.com>
2020-05-28 17:41:49 -0600

    Add methods for Iteration 2

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,4 +1,8 @@
 require 'csv'
+require_relative 'games'
+require_relative 'teams'
+require_relative 'game_teams'
+
 class StatTracker
   attr_reader :games, :teams, :game_teams
   def self.from_csv(data)
@@ -12,5 +16,125 @@ class StatTracker
     @games      = games_collection
     @teams      = teams_collection
     @game_teams = game_teams_collection
+  end
+
+  # Game Statistics
+  def highest_total_score
+    @games.map {|game| game.away_goals + game.home_goals}.max
+  end
+
+  def lowest_total_score
+    @games.map {|game| game.away_goals + game.home_goals}.min
+  end
+
+  def percentage_home_wins
+    # Returns a float
+  end
+
+  def percentage_visitor_wins
+    # Returns a float
+  end
+
+  def percentage_ties
+    # Returns a float
+  end
+
+  def count_of_games_by_season
+    # Returns a hash
+  end
+
+  def average_goals_per_game
+    # Returns a float
+  end
+
+  def average_goals_by_season
+    # Returns a hash
+  end
+
+  # League Statistics
+  def count_of_teams
+    # Returns an integer
+  end
+
+  def best_offense
+    # Returns a string
+  end
+
+  def worst_offense
+    # Returns a string
+  end
+
+  def highest_scoring_visitor
+    # Returns a string
+  end
+
+  def highest_scoring_home_team
+    # Returns a string
+  end
+
+  def lowest_scoring_visitor
+    # Returns a string
+  end
+
+  def lowest_scoring_home_team
+    # Returns a string
+  end
+
+  # Season Statistics - All methods take a season id as an argument
+  def winningest_coach(season_id)
+    # Returns a string
+  end
+
+  def worst_coach(season_id)
+    # Returns a string
+  end
+
+  def most_accurate_team(season_id)
+    # Returns a string
+  end
+
+  def least_accurate_team(season_id)
+    # Returns a string
+  end
+
+  def most_tackles(season_id)
+    # Returns a string
+  end
+
+  def fewest_tackles(season_id)
+    # Returns a string
+  end
+
+  # Team Statistics - All methods take a team id as an argument
+  def team_info(team_id)
+    # Returns a hash
+  end
+
+  def best_season(team_id)
+    # Returns a string
+  end
+
+  def worst_season(team_id)
+    # Returns a string
+  end
+
+  def average_win_percentage(team_id)
+    # Returns a float
+  end
+
+  def most_goals_scored(team_id)
+    # Returns an integer
+  end
+
+  def fewest_goals_scored(team_id)
+    # Returns an integer
+  end
+
+  def favorite_oppenent(team_id)
+    # Returns a string
+  end
+
+  def rival(team_id)
+    # Returns a string
   end
 end

f6f1089fecf96cbd80d75923d3b767cabc02846f
Dani Coleman <danicoleman00@gmail.com>
2020-06-01 14:44:33 -0600

    Add new assertions for actual csv files

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -10,9 +10,9 @@ require 'pry'
 
 class StatTrackerTest < MiniTest::Test
   def setup
-    @stat_tracker = StatTracker.from_csv({games:      './fixture/games_fixture.csv',
+    @stat_tracker = StatTracker.from_csv({games:      './data/games.csv',
                                           teams:      './data/teams.csv',
-                                          game_teams: './fixture/game_teams_fixture.csv'})
+                                          game_teams: './data/game_teams.csv'})
   end
 
   def test_it_exists
@@ -20,54 +20,56 @@ class StatTrackerTest < MiniTest::Test
   end
 
   def test_it_generates_collections
-    assert_equal 29, @stat_tracker.games.count
-    assert_equal 34, @stat_tracker.game_teams.count
+    assert_equal 7441, @stat_tracker.games.count
+    assert_equal 14882, @stat_tracker.game_teams.count
     assert_equal 32, @stat_tracker.teams.count
   end
 
   def test_it_can_access_data
-    assert_equal "5", @stat_tracker.games[4].home_team_id
+    assert_equal "6", @stat_tracker.games[4].home_team_id
     assert_equal "Chicago Fire", @stat_tracker.teams[1].team_name
     assert_equal "John Tortorella", @stat_tracker.game_teams[5].head_coach
   end
 
   #Game Stats Tests
   def test_it_can_get_highest_total_score
-    assert_equal 5, @stat_tracker.highest_total_score
+    assert_equal 11, @stat_tracker.highest_total_score
   end
   def test_it_can_get_lowest_total_score
-    assert_equal 1, @stat_tracker.lowest_total_score
+    assert_equal 0, @stat_tracker.lowest_total_score
   end
   def test_it_has_percentage_of_home_wins
-    assert_equal 0.59, @stat_tracker.percentage_home_wins
+    assert_equal 0.44, @stat_tracker.percentage_home_wins
   end
   def test_percentage_visitor_wins
-    assert_equal 0.29, @stat_tracker.percentage_visitor_wins
+    assert_equal 0.36, @stat_tracker.percentage_visitor_wins
   end
   def test_percentage_ties
-    assert_equal 0.12, @stat_tracker.percentage_ties
+    assert_equal 0.2, @stat_tracker.percentage_ties
   end
   def test_count_of_games_by_season
     expected = {
-      "20122013" => 21,
-      "20172018" => 2,
-      "20162017" => 2,
-      "20152016" => 2,
-      "20132014" => 2
+      "20122013"=>806,
+      "20162017"=>1317,
+      "20142015"=>1319,
+      "20152016"=>1321,
+      "20132014"=>1323,
+      "20172018"=>1355
     }
     assert_equal expected, @stat_tracker.count_of_games_by_season
     #assert_includes "20122013" , @stat_tracker.count_of_games_by_season.key?[0]
   end
   def test_average_goals_per_game
-    assert_equal 3.65, @stat_tracker.average_goals_per_game
+    assert_equal 4.22, @stat_tracker.average_goals_per_game
   end
   def test_average_goals_by_season
     expected = {
-      "20122013"=>3.71,
-      "20172018"=>4.0,
-      "20162017"=>4.0,
-      "20152016"=>4.0,
-      "20132014"=>4.0
+      "20122013"=>4.12,
+      "20162017"=>4.23,
+      "20142015"=>4.14,
+      "20152016"=>4.16,
+      "20132014"=>4.19,
+      "20172018"=>4.44
     }
     assert_equal expected, @stat_tracker.average_goals_by_season
   end
@@ -77,11 +79,11 @@ class StatTrackerTest < MiniTest::Test
   end
 
   def test_best_offense
-    assert_equal 'FC Dallas', @stat_tracker.best_offense
+    assert_equal "Reign FC", @stat_tracker.best_offense
   end
 
   def test_worst_offense
-    assert_equal 'Sporting Kansas City', @stat_tracker.worst_offense
+    assert_equal "Utah Royals FC", @stat_tracker.worst_offense
   end
 
   def test_highest_scoring_visitor
@@ -89,15 +91,15 @@ class StatTrackerTest < MiniTest::Test
   end
 
   def test_highest_scoring_home_team
-    assert_equal "LA Galaxy", @stat_tracker.highest_scoring_home_team
+    assert_equal "Reign FC", @stat_tracker.highest_scoring_home_team
   end
 
   def test_lowest_scoring_visitor
-    assert_equal "Sporting Kansas City", @stat_tracker.lowest_scoring_visitor
+    assert_equal "San Jose Earthquakes", @stat_tracker.lowest_scoring_visitor
   end
 
   def test_lowest_scoring_home_team
-    assert_equal "Sporting Kansas City", @stat_tracker.lowest_scoring_home_team
+    assert_equal "Utah Royals FC", @stat_tracker.lowest_scoring_home_team
   end
 
   # Season Stats
@@ -106,23 +108,23 @@ class StatTrackerTest < MiniTest::Test
   end
 
   def test_it_has_the_worst_coach
-    assert_equal "John Tortorella", @stat_tracker.worst_coach("20122013")
+    assert_equal "Martin Raymond", @stat_tracker.worst_coach("20122013")
   end
 
   def test_it_has_most_accurate_team
-    assert_equal "FC Dallas", @stat_tracker.most_accurate_team("20122013")
+    assert_equal "DC United", @stat_tracker.most_accurate_team("20122013")
   end
 
   def test_it_has_least_accurate_team
-    assert_equal "Sporting Kansas City", @stat_tracker.least_accurate_team("20122013")
+    assert_equal "New York City FC", @stat_tracker.least_accurate_team("20122013")
   end
 
   def test_most_tackles
-    assert_equal "FC Dallas", @stat_tracker.most_tackles("20122013")
+    assert_equal "FC Cincinnati", @stat_tracker.most_tackles("20122013")
   end
 
   def test_it_has_fewest_tackles
-    assert_equal "Houston Dynamo", @stat_tracker.fewest_tackles("20122013")
+    assert_equal "Atlanta United", @stat_tracker.fewest_tackles("20122013")
   end
 
 # Team Stats
@@ -136,21 +138,21 @@ class StatTrackerTest < MiniTest::Test
   end
 
   def test_it_has_best_season
-    assert_equal "", @stat_tracker.best_season("6")
+    assert_equal "20132014", @stat_tracker.best_season("6")
   end
 
   def test_it_has_worst_season
-    assert_equal "", @stat_tracker.best_season("6")
+    assert_equal "20132014", @stat_tracker.best_season("6")
   end
 
   def test_average_win_percentage
-    assert_equal 1.0, @stat_tracker.average_win_percentage("6")
-    assert_equal 0.0, @stat_tracker.average_win_percentage("3")
-    assert_equal 0.33, @stat_tracker.average_win_percentage("16")
+    assert_equal 0.49, @stat_tracker.average_win_percentage("6")
+    assert_equal 0.43, @stat_tracker.average_win_percentage("3")
+    assert_equal 0.44, @stat_tracker.average_win_percentage("16")
   end
 
   def test_most_goals_scored
-    assert_equal 4, @stat_tracker.most_goals_scored("6")
+    assert_equal 6, @stat_tracker.most_goals_scored("6")
   end
 
   def test_fewest_goals_scored
@@ -158,10 +160,10 @@ class StatTrackerTest < MiniTest::Test
   end
 
   def test_it_has_favorite_opponent
-    assert_equal "FC Dallas", @stat_tracker.favorite_opponent("3")
+    assert_equal "Montreal Impact", @stat_tracker.favorite_opponent("3")
   end
 
   def test_it_has_a_rival
-    assert_equal "Houston Dynamo", @stat_tracker.rival("6")
+    assert_equal "Real Salt Lake", @stat_tracker.rival("6")
   end
 end
